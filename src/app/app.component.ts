c862abb613e66fe64b616883679d6b24f3053ebd
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-30 08:45:30 +0530

    works commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { products } from './models/products';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ICPLOS-Frontend';
+  products: products[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<products[]>('https://localhost:7184/api/products').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete')
+    });
+  }
 }
---
+++ src/app/models/products.ts
@@ -0,0 +1,11 @@
+export type products = Products[]
+
+export interface Products {
+  id: string
+  name: string
+  description: string
+  price: number
+  pictureUrl: string
+  createdAt: string
+  updatedAt: string
+}
